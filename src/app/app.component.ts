c865d06248adf262d573ee70cc9e2234aaa5a564
vishal kumar <vishalkumar36565@gmail.com>
2023-10-31 20:48:10 +0530

    Counter App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'counterApp';
+  count = 0;
+
+  reset() {
+    this.count = 0;
+  }
+
+  increment() {
+    this.count += 1;
+  }
+
+  decrement() {
+    if (this.count > 0) {
+      this.count -= 1;
+    }
+  }
 }
